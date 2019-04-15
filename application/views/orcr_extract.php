6da2f521b10a31c7c64e6b01f715b93d0ebc38fd
Jake Azura <jmcazura@motortrade.com.ph>
2019-04-15 15:29:19 +0800

    Added branch_name Orcr_Extract

--- application/views/orcr_extract.php
+++ application/views/orcr_extract.php
@@ -9,7 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="pull-left">ORCR Extract</div>
       </div>
       <div class="block-content collapse in">
-      	<form method="post" action="/orcr_extract/csv" target="_blank" class="form-horizontal">
+        <form method="post" action="<?php echo base_url(); ?>orcr_extract/csv" target="_blank" class="form-horizontal">
           <div class="control-group">
             <div class="control-label">For Extraction</div>
             <div class="controls"><?php print $count.' Units'; ?></div>
@@ -22,36 +22,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
           <hr>
 
-					<table class="table">
-						<thead>
-							<tr>
-								<th>Batch #</th>
-								<th>Timestamp</th>
-								<th># of Units</th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php
-							foreach ($table as $row) {
-								print '<tr>';
-								print '<td>Batch-'.$row->batch_no.'</td>';
-								print '<td>'.$row->extract_date.'</td>';
-								print '<td>'.$row->count.'</td>';
-								print '<td><input type="submit" name="extract" value="Extract" class="btn btn-success" data-batch="'.$row->batch_no.'"></td>';
-								print '</tr>';
-							}
+	  <table class="table">
+	  	<thead>
+	  		<tr>
+	  			<th>Batch #</th>
+	  			<th>Timestamp</th>
+	  			<th># of Units</th>
+	  		</tr>
+	  	</thead>
+	  	<tbody>
+	  		<?php
+	  		foreach ($table as $row) {
+	  			print '<tr>';
+	  			print '<td>Batch-'.$row->batch_no.'</td>';
+	  			print '<td>'.$row->extract_date.'</td>';
+	  			print '<td>'.$row->count.'</td>';
+	  			print '<td><input type="submit" name="extract" value="Extract" class="btn btn-success" data-batch="'.$row->batch_no.'"></td>';
+	  			print '</tr>';
+	  		}
 
-							if (empty($table)) {
-								print '<tr>';
-								print '<td>No batch result.</td>';
-								print '<td></td>';
-								print '<td></td>';
-								print '</tr>';
-							}
-							?>
-						</tbody>
-					</table>
-				</form>
+	  		if (empty($table)) {
+	  			print '<tr>';
+	  			print '<td>No batch result.</td>';
+	  			print '<td></td>';
+	  			print '<td></td>';
+	  			print '</tr>';
+	  		}
+	  		?>
+	  	</tbody>
+	  </table>
+        </form>
       </div>
     </div>
   </div>
