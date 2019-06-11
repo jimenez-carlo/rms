d1e8fe2556b00efb896e609ccc0a32872b30e02f
Jake Azura <jmcazura@motortrade.com.ph>
2019-06-11 19:13:54 +0800

    Line break

--- application/controllers/Lto_pending.php
+++ application/controllers/Lto_pending.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Lto_pending extends MY_Controller { 
+class Lto_pending extends MY_Controller {
 
   public function __construct() {
 		parent::__construct();
