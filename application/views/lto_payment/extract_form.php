ca67d9cdcf9eb26fdcc6c6baadac4d8c5d25058b
Jake Azura <jmcazura@motortrade.com.ph>
2019-06-13 08:41:04 +0800

    Bug due to migration.
    Fix form action.
    Rearrange indentation.

--- application/views/lto_payment/extract_form.php
+++ application/views/lto_payment/extract_form.php
@@ -2,38 +2,38 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div class="container-fluid">
-	<div class="row-fluid">
-		<div class="block">
-			<div class="navbar navbar-inner block-header">
-				<div class="pull-left">LTO Payment</div>
-			</div>
-			<div class="block-content collapse in">
-				<form class="form-horizontal" method="post" action="/lto_payment/csv" target="_blank">
-					<fieldset>
-            <div class="control-group span5">
-              <div class="control-label">Pending Date</div>
-              <div class="controls">
-                <span style="display: inline-block; width: 50px">From:</span>
-                <?php print form_input('date_from', set_value('date_from', date('Y-m-d', strtotime('-1 days'))), array('class' => 'datepicker')); ?>
-                <br>
-                <span style="display: inline-block; width: 50px">To:</span>
-                <?php print form_input('date_to', set_value('date_to', date('Y-m-d', strtotime('-1 days'))), array('class' => 'datepicker')); ?>
-              </div>
-            </div>
+        <div class="row-fluid">
+                <div class="block">
+                        <div class="navbar navbar-inner block-header">
+                                <div class="pull-left">LTO Payment</div>
+                        </div>
+                        <div class="block-content collapse in">
+                                <form class="form-horizontal" method="post" action="<?php echo base_url(); ?>lto_payment/csv" target="_blank">
+                                        <fieldset>
+                                                <div class="control-group span5">
+                                                  <div class="control-label">Pending Date</div>
+                                                  <div class="controls">
+                                                    <span style="display: inline-block; width: 50px">From:</span>
+                                                    <?php print form_input('date_from', set_value('date_from', date('Y-m-d', strtotime('-1 days'))), array('class' => 'datepicker')); ?>
+                                                    <br>
+                                                    <span style="display: inline-block; width: 50px">To:</span>
+                                                    <?php print form_input('date_to', set_value('date_to', date('Y-m-d', strtotime('-1 days'))), array('class' => 'datepicker')); ?>
+                                                  </div>
+                                                </div>
 
-						<div class="control-group span5">
-							<div class="control-label">Company</div>
-							<div class="controls">
-								<?php print form_dropdown('company', $company, set_value('company', 1)); ?>
-							</div>
-						</div>
+                                                <div class="control-group span5">
+                                                        <div class="control-label">Company</div>
+                                                        <div class="controls">
+                                                                <?php print form_dropdown('company', $company, set_value('company', 1)); ?>
+                                                        </div>
+                                                </div>
 
-						<div class="form-actions span5">
-							<input type="submit" class="btn btn-success" value="Extract" name="extract" onclick="return confirm('Please make sure all information are correct before proceeding. Continue?')">
-						</div>
-					</fieldset>
-				</form>
-			</div>
-		</div>
-	</div>
+                                                <div class="form-actions span5">
+                                                        <input type="submit" class="btn btn-success" value="Extract" name="extract" onclick="return confirm('Please make sure all information are correct before proceeding. Continue?')">
+                                                </div>
+                                        </fieldset>
+                                </form>
+                        </div>
+                </div>
+        </div>
 </div>
