03c5876c5c7936b0dbf7efa8e4899e01a9f36f5c
jake_azura <jake.azura@motortrade.com.ph>
2019-10-25 15:09:15 +0800

    Show images in page.

--- application/views/return_fund/view.php
+++ application/views/return_fund/view.php
@@ -2,16 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div class="container-fluid form-horizontal">
-	<div class="row-fluid">
+  <div class="row-fluid">
     <div class="block">
       <div class="navbar navbar-inner block-header">
         <div class="pull-left">Return Fund # <?php print $return->rfid; ?></div>
       </div>
       <div class="block-content collapse in">
         <div class="row-fluid">
-
           <div class="span5">
-
             <?php if (empty($return->liq_date) && $_SESSION['position'] == 3) { ?>
             <form method="post" class="form-horizontal">
               <div class="form-actions">
@@ -19,7 +17,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
               </div>
             </form>
             <?php } ?>
-
             <div class="control-group">
               <div class="control-label">Reference #</div>
               <div class="controls text"><?php print $return->reference; ?></div>
@@ -29,17 +26,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
               <div class="controls text"><?php print number_format($return->amount, 2, '.', ','); ?></div>
             </div>
             <div class="control-group">
-              <div class="control-label">Deposit Slip</div>
-              <div class="controls text"><?php print '<a href="'.base_url().'rms_dir/deposit_slip/'.$return->rfid.'/'.$return->slip.'" target="_blank">'.$return->slip.'</a>'; ?></div>
-            </div>
-            <div class="control-group">
               <div class="control-label">Date Liquidated</div>
               <div class="controls text"><?php print (empty($return->liq_date)) ? '-' : $return->liq_date; ?></div>
             </div>
           </div>
-
         </div>
-			</div>
-		</div>
-	</div>
+      </div>
+    </div>
+  </div>
+  <img class="img-rounded" <?php echo 'src="'.base_url().'rms_dir/deposit_slip/'.$return->rfid.'/'.$return->slip.'"  alt="'.$return->slip.'"'; ?>>
 </div>
