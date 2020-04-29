ef4e3ebbf674c1693a05f781c9d3bd981157391a
Jake Marfon Chito Azura <jake.azura@motortrade.com.ph>
2020-04-29 16:20:04 +0800

    Fragment identifier Selected reference number is always on top

--- application/views/orcr_checking/list.php
+++ application/views/orcr_checking/list.php
@@ -8,28 +8,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </style>
 
 <div class="container-fluid form-horizontal">
-	<div class="row-fluid">
+  <div class="row-fluid">
     <!-- block -->
-    <div class="block span2" style="min-height: 86vh; overflow-y: auto;">
-      <div class="navbar navbar-inner block-header">
-        <div class="pull-left">CA Reference</div>
+    <div class="block span2">
+      <div class="navbar navbar-inner block-header" style="width:79%; position:relative; z-index:1;">
+        <div class="pull-left">Reference Number</div>
       </div>
-      <div class="block-content collapse in">
+      <div class="block-content collapse in" style="margin:0; height:690px; overflow-y:auto;">
         <form class="form-horizontal list-form" method="post" style="margin:0;">
           <?php //print form_hidden('tid', 0); ?>
-          <?php print form_hidden('vid', 0); ?>
+          <?php //print form_hidden('vid', 0); ?>
+          <input id="for_check_id" type="hidden">
           <table class="table" style="margin:0;">
             <tbody>
               <?php
-                // echo '<pre>';
-                // print_r($ca_refs);
                 if (empty($ca_refs)) {
                   print '<tr><td>No result.</td></tr>';
                 } else {
+
                   foreach ($ca_refs as $ca_reference) {
-                    echo '<tr>';
+                    $style = ($reference_selected === $ca_reference['reference']) ? 'style="background:mediumaquamarine;"' : '';
+                    echo '<tr id="'.$ca_reference['reference'].'" '.$style.'>';
                     echo   '<td>
-                      <a class="btn btn-success btn-mini" onclick="vid('.$ca_reference['vid'].')">
+                      <a class="btn btn-success btn-mini" onclick="get_sales('.$ca_reference['id'].', \''.$ca_reference['budget_type'].'\', \''.$ca_reference['reference'].'\')">
                       <i class="icon-edit"></i>
                       </a> '.$ca_reference['reference'].
                       '</td>';
@@ -62,7 +63,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <!-- block -->
     <div class="block span10">
-      <div class="navbar navbar-inner block-header">
+      <div class="navbar navbar-inner block-header" style="position:relative;">
         <div class="pull-left">Transaction <?php echo (isset($ca_ref)) ? '# '.$ca_ref['reference'] : ''; ?></div>
       </div>
       <div class="block-content collapse in">
@@ -85,8 +86,9 @@ function tid(_tid) {
   $('.list-form').submit();
 }
 
-function vid(vid) {
-  $('.list-form input').val(vid);
+function get_sales(id, budget_type, reference) {
+  $('#for_check_id').prop({'name': budget_type, 'value': id});
+  $('.list-form').attr('action', 'orcr_checking#'+reference);
   $('.list-form').submit();
 }
 </script>
