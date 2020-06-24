b3fa0bd66a22b7e6dfdfbc839e7cf422770bfa38
Jake Marfon Chito Azura <jake.azura@motortrade.com.ph>
2020-06-24 10:49:47 +0800

    Fix SAP Upload template

--- application/models/Batch_model.php
+++ application/models/Batch_model.php
@@ -67,51 +67,7 @@ class Batch_model extends CI_Model{
 
 	public function sap_upload($subid)
 	{
-          $sql1 = <<<QRY
-            SELECT
-              b.bid, b.topsheet, SUBSTR(b.post_date, 1, 10) AS batch_post_date, b.trans_no,
-              b.doc_no, b.misc, b.status, b.download_date, t.tid, t.user,
-              SUBSTR(t.post_date, 1, 10) AS topsheet_post_date, t.trans_no,
-              t.date, t.meal, t.photocopy, t.transportation,
-              t.others, t.others_specify, t.status, t.misc_status, t.print,
-              t.transmittal, t.print_date, t.transmittal_date,
-              f.acct_number AS account_key,
-              CASE WHEN t.company = 2 THEN 6 ELSE t.company END AS bcode,
-              CASE
-                WHEN t.region = 1 THEN 'NCR'
-                WHEN t.region = 2 THEN 'Region 1'
-                WHEN t.region = 3 THEN 'Region 2'
-                WHEN t.region = 4 THEN 'Region 3'
-                WHEN t.region = 5 THEN 'Region 4A'
-                WHEN t.region = 6 THEN 'Region 4B'
-                WHEN t.region = 7 THEN 'Region 5'
-                WHEN t.region = 8 THEN 'Region 6'
-                WHEN t.region = 9 THEN 'Region 7'
-                WHEN t.region = 10 THEN 'Region 8'
-                WHEN t.region = 11 THEN 'Region IX'
-                WHEN t.region = 12 THEN 'Region X'
-                WHEN t.region = 13 THEN 'Region XI'
-                WHEN t.region = 14 THEN 'Region XII'
-                WHEN t.region = 15 THEN 'Region XIII'
-              END AS region,
-              CASE
-                WHEN t.company = 1 THEN 'MNC'
-                WHEN t.company = 2 THEN 'MTI'
-                WHEN t.company = 6 THEN 'MTI'
-                WHEN t.company = 3 THEN 'HPTI'
-                WHEN t.company = 8 THEN 'MDI'
-              END AS company
-             FROM
-               tbl_batch b
-                   INNER JOIN
-               tbl_topsheet t ON b.topsheet = t.tid
-                  INNER JOIN
-               tbl_fund f ON t.region = f.region
-               WHERE
-                 bid = $subid
-QRY;
-
-              $sql2 = <<<QRY
+              $sql = <<<SQL
                 SELECT
                   DATE_FORMAT(s.date_sold, '%m/%d/%Y') AS post_date,
                   CASE c.company_code
@@ -124,15 +80,12 @@ QRY;
                     WHEN 'Free Registration'  THEN '215450'
                     ELSE CONCAT('219', SUBSTR(bcode, 1, 3))
                   END AS sap_code,
-                  s.si_no,
-                  s.ar_no,
-                  s.registration_type,
-                  f.acct_number AS account_key,
+                  s.si_no, s.ar_no, s.amount AS ar_amount,
+                  s.registration_type, f.acct_number AS account_key,
                   s.registration + s.tip AS regn_expense, -- NEED TO ADD misc expenses
                   CONCAT(s.bcode, '000') AS branch_code,
                   IFNULL(lp.reference, v.reference) AS reference_number,
-                  cust.cust_code,
-                  CONCAT(cust.last_name, ', ', cust.first_name) AS customer_name
+                  cust.cust_code, CONCAT(cust.last_name, ', ', cust.first_name) AS customer_name
                 FROM
                   tbl_sap_upload_batch sub
 	        JOIN
@@ -153,45 +106,8 @@ QRY;
                   tbl_voucher v ON s.voucher = v.vid
                 WHERE
                   subid = $subid
-QRY;
-                $batch = $this->db->query($sql2)->result_array();
-                //$batch = $this->db->query($sql1)->row();
-                //echo '<pre>'; var_dump($this->db->last_query()); echo '</pre>'; die();
-                // echo '<pre>'; var_dump($batch); echo '</pre>'; die();
-
-                //switch ($batch->region) {
-                //  case 'NCR':
-                //  case 'Region 6':
-
-                //    $batch->sales = $this->db->query("
-                //      SELECT
-                //        *, s.amount as amount
-                //      FROM
-                //        tbl_sales s
-                //      INNER JOIN
-                //        tbl_customer ON customer = cid
-                //      INNER JOIN
-                //        tbl_lto_payment ON lto_payment = lpid
-                //      WHERE
-                //        batch = ".$subid
-                //    )->result_object();
-                //    break;
-
-                //  default:
-                //    $batch->sales = $this->db->query("
-                //      SELECT
-                //        *, s.amount as amount
-                //      FROM
-                //        tbl_sales s
-                //      INNER JOIN
-                //        tbl_customer ON customer = cid
-                //      INNER JOIN
-                //        tbl_voucher ON voucher = vid
-                //      WHERE
-                //        batch = ".$subid
-                //    )->result_object();
-                //    break;
-                //}
+SQL;
+                $batch = $this->db->query($sql)->result_array();
 
                 $misc_exp_qry = <<<SQL
                   SELECT
--- application/views/sap_upload/sap.php
+++ application/views/sap_upload/sap.php
@@ -1,4 +1,5 @@
 <?php
+// echo '<pre>'; var_dump($batch); echo '</pre>'; die();
 $list = array();
 $line = array(
   "Counter V_COUNTER", "Document Date BKPF-BLART", "Posting Date BKPF-BUDAT",
@@ -17,7 +18,6 @@ $ctr = 0;
 foreach ($batch as $sales)
 {
 	$ctr++;
-
         $misc_expense = $misc_expenses[$sales['reference_number']] ?? 0;
         $regn_and_misc_expense = $sales['regn_expense'] + $misc_expense;
 
@@ -43,6 +43,7 @@ foreach ($batch as $sales)
             break;
 
           case 'Regular Regn. Paid':
+          case 'Regn. under NIA':
 	    //debit AR
 	    $line = array(
               $ctr,
@@ -63,11 +64,20 @@ foreach ($batch as $sales)
             break;
 
           case 'With Regn. Subsidy':
+            $si_exp = 0;
+            $ar_exp = 0;
+            if ($regn_and_misc_expense > $sales['ar_amount']) {
+              $si_exp = $regn_and_misc_expense - $sales['ar_amount'];
+              $ar_exp = $sales['ar_amount'];
+            } elseif ($regn_and_misc_expense <= $sales['ar_amount']) {
+              $ar_exp = $regn_and_misc_expense;
+            }
+
 	    //debit SI
 	    $line = array(
               $ctr,
               $sales['post_date'],$sales['post_date'],'KR',$sales['c_code'],'PHP','',$sales['si_no'],'','31',$sales['account_key'],'',
-              number_format($regn_and_misc_expense, 2, '.', ''),
+              number_format($si_exp, 2, '.', ''),
               '','','','','','','','',$sales['branch_code'],$sales['branch_code'],'','',$sales['reference_number'],$sales['cust_code'],
 	    );
 	    $list[] = $line;
@@ -76,27 +86,28 @@ foreach ($batch as $sales)
 	    $line = array(
 	    	$ctr,
 	    	$sales['post_date'],$sales['post_date'],'KR',$sales['c_code'],'PHP','',$sales['si_no'],'','40',$sales['sap_code'],'',
-	    	number_format($regn_and_misc_expense, 2, '.', ''),
+	    	number_format($si_exp, 2, '.', ''),
                     '','','','','','','','',$sales['branch_code'],$sales['branch_code'],'','',$sales['customer_name'],$sales['cust_code'],
 	    );
 	    $list[] = $line;
 
+            $ctr++;
 	    //debit AR
 	    $line = array(
               $ctr,
               $sales['post_date'],$sales['post_date'],'KR',$sales['c_code'],'PHP','',$sales['ar_no'],'','31',$sales['account_key'],'',
-              number_format($sales['regn_expense'], 2, '.', ''),
+              number_format($ar_exp, 2, '.', ''),
               '','','','','','','','',$sales['branch_code'],$sales['branch_code'],'','',$sales['reference_number'],$sales['cust_code'],
 	    );
 	    $list[] = $line;
 
 	    //credit AR
-	    $line = array(
-	    	$ctr,
-	    	$sales['post_date'],$sales['post_date'],'KR',$sales['c_code'],'PHP','',$sales['ar_no'],'','40',$sales['sap_code'],'',
-	    	number_format($sales['regn_expense'], 2, '.', ''),
-                    '','','','','','','','',$sales['branch_code'],$sales['branch_code'],'','',$sales['customer_name'],$sales['cust_code'],
-	    );
+            $line = array(
+              $ctr,
+              $sales['post_date'],$sales['post_date'],'KR',$sales['c_code'],'PHP','',$sales['ar_no'],'','40',$sales['sap_code'],'',
+              number_format($ar_exp, 2, '.', ''),
+              '','','','','','','','',$sales['branch_code'],$sales['branch_code'],'','',$sales['customer_name'],$sales['cust_code'],
+            );
 	    $list[] = $line;
             break;
         }
